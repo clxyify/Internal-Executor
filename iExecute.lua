1c99e1502a4cf3b8b61c35e21d5d09067352b223
clxyify <98536033+clxyify@users.noreply.github.com>
2022-05-25 19:59:16 -0400

    Create iExecute.lua

---
+++ iExecute.lua
@@ -0,0 +1,16 @@
+function iExecute(source)--use 'source' as our target script
+	local src = loadstring(source.."\n\nprint'Clxyify Internal Executor - plz don't skid ;)'")()--acts as a function
+	src()--run the script as a function
+end
+
+--[[
+local scripto = "print'clxyify internal ui'"
+local scripto2 = scripto.." print'clxyify..aura7ufahjwty873yhgkff'"
+local scripto3 = math.random(1,2)
+
+if (scripto3 == 1) then
+iExecute(scripto)
+else
+iExecute(scripto2)
+end
+]]
